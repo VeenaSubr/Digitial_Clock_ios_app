5fd610d3ce30304a16333b1683b05861f054320d
veenasubr@gmail.com <veenasubr@gmail.com>
2015-10-23 16:00:05 -0700

    clock is working

--- DigitalClock/ViewController.swift
+++ DigitalClock/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTimeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTimeLabel.text = ""
+        _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updateClock"), userInfo: nil, repeats: true)
+        
+        
+    }
+    func updateClock(){
+        
+        let date = NSDate()
+        let calender = NSCalendar.currentCalendar()
+    
+        let components = calender.components([.Hour, .Minute, .Second], fromDate: date)
+        
+        var hour = components.hour > 12 ? components.hour - 12 : components.hour
+        
+        hour = hour == 0 ? 12 : hour
+        
+        let hourString = hour > 9 ? "\(hour)" : "0\(hour)"
+        let minuteString = components.minute > 9 ? "\(components.minute)" : "0\(components.minute)"
+        let secondString = components.second > 9 ? "\(components.second)" : "0\(components.second)"
+        let am = components.hour > 12 ? "AM" : "PM"
+        
+        myTimeLabel.text = "\(hourString) : \(minuteString) : \(secondString) \(am)"
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
